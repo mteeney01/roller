fe4f2a50d307099826b0ad51653572e98883fbbb
Matt Teeney <matthew.teeney@civica.co.uk>
2018-05-09 15:42:08 +0100

    Adding simple roll method, with static DIE values

---
+++ roller.js
@@ -0,0 +1,29 @@
+var exports = module.exports = {};
+
+exports.DIE = {
+    D4: 4,
+    D6: 6,
+    D8: 8,
+    D10: 10,
+    D12: 12,
+    D20: 20
+};
+
+exports.roll = function(amount, die) {
+    let result = [];
+
+    for (let i = 0; i < amount; i++) {
+        result.push(getRandomIntInclusive(1, die));
+    }
+
+    return {
+        rolls: result
+    };
+};
+
+/* From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive */
+var getRandomIntInclusive = function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+};
